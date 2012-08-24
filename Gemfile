828fa91e0f45fe6acde370c2d805e1f10103da17
Ivan Stroganov <knowhere@gmail.com>
2012-08-23 17:57:30 -0700

    app with search controller

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.7'
+gem 'pg'
+gem 'thin'
+gem 'haml-rails'
+gem 'jquery-rails'
+gem 'heroku'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'jquery-ui-rails'
+  gem 'jquery-rails'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'quiet_assets'
+  gem 'jazz_hands'
+end
+
+group :develoment, :test do
+  gem 'nyan-cat-formatter'
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tapjoy::Application.config.secret_token = '236ccdb78d04ccda866a69d9c051bcf6bbdd925fc4b5b04dfd81f4fdeba954b0840e3dda75f6a8f5685839518c7045d676d6affb754a7480895a30fe2c980aa5'
