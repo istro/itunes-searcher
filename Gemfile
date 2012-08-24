2140810448d793e28104ac044bf37b3904caa9a5
Ivan Stroganov <knowhere@gmail.com>
2012-08-24 15:49:53 -0700

    updated styling and ajax call to itunes search api

--- Gemfile
+++ Gemfile
@@ -11,7 +11,6 @@ group :assets do
   gem 'bootstrap-sass'
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
-  gem 'jquery-ui-rails'
   gem 'jquery-rails'
   gem 'uglifier', '>= 1.0.3'
 end
--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,20 +1,37 @@
-$(function() {
+$(function(){
+
+  var parseResponse = function(response) {
+    results = response["results"]
+    for(i=0; results.length; i++) {
+      var name = "<li>"+results[i]["trackName"]+"</li>";
+      console.log(name);
+      $("#search_results").prepend(name)
+    }
+    console.log(results.length)
+    console.log('all your base are belong to us')
+  };
 
   $('.search').click(function(event){
-    $.ajax({
-      url: "http://itunes.apple.com/search",
+    var url = "http://itunes.apple.com/search"
+    $.ajax(url, {
       dataType: "jsonp",
       crossDomain: true,
       data: {
+        media: "music", 
         term: $(".search_term").val(),
         limit: 25
       },
-      success: console.log("success!"),
-      error: function(){
-        console.log("oops")
-      }
+      jsonpCallback: "parseResponse",
+      success: parseResponse
     })
     event.preventDefault();
   });
 
+  $('#myTab a').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+  })
+
+  $('#myTab a:first').tab('show');
+  
 });
