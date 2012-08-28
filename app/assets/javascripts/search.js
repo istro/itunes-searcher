3a50bc98b6ecae91955dbabf5f875da4c627e94f
Ivan Stroganov <knowhere@gmail.com>
2012-08-28 16:19:12 -0700

    sessionwide faves working

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,9 +3,12 @@ $(function(){
   var parseResponse = function(response) {
     list_of_results = "";
     results = response["results"];
+    showResults()
+  };
+
+  var showResults = function() {
     for(i=0; i < results.length; i++) {
-      // soundManager.createSound(results[i]["trackId"], results[i]["previewUrl"]);
-      var song = '<div class="row"><li><div class="ui360"><a href="'+results[i]["previewUrl"]+'"></a></div><div class="name">'+results[i]["trackName"]+'</div><div class="fav"><img src="images/star.svg"</div></li></div>';
+      var song = '<tr><td><div class="ui360" style="width: 30px"><a href="'+results[i]["previewUrl"]+'"></a></div></td><td>'+results[i]["trackName"]+'</td><td><span class="artwork"><img src="'+results[i]["artworkUrl60"]+'"></span>'+results[i]["collectionName"]+'</td><td>'+results[i]["artistName"]+'</td><td><div class="addfave" id="'+results[i]["trackId"]+'"><img src="images/star.svg"></div></td></tr></div>';
       console.log(song);
       list_of_results += song
     };
@@ -14,7 +17,19 @@ $(function(){
     threeSixtyPlayer = new ThreeSixtyPlayer();
     soundManager.onready(threeSixtyPlayer.init);
     console.log('all your base are belong to us')
-  };
+  }
+
+  $("#search_results").delegate(".addfave", "click", function(event){
+    console.log(this.id);
+    // add to user's faves by id
+    for(i=0; i<results.length; i++) {
+      if (results[i]["trackId"] == this.id) {
+        console.log("got it!")
+        var song = '<tr><td><div class="ui360" style="width: 30px"><a href="'+results[i]["previewUrl"]+'"></a></div></td><td>'+results[i]["trackName"]+'</td><td><span class="artwork"><img src="'+results[i]["artworkUrl60"]+'"></span>'+results[i]["collectionName"]+'</td><td>'+results[i]["artistName"]+'</td><td><div class="addfave" id="'+results[i]["trackId"]+'"><img src="images/star.svg"></div></td></tr></div>';
+        $("#faves").append(song)
+      }
+    }
+  });
 
   $('.search').click(function(event){
     var url = "http://itunes.apple.com/search"
